060f879fef5d048fbcb8512709b5c2eee9d6fda6
Grzegorz Krajewski <g.krajewski82@gmail.com>
2022-07-11 17:48:28 +0200

    First commit

---
+++ src/test/java/com/kodilla/foodpairingbackend/FoodPairingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.foodpairingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPairingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
